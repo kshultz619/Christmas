d8edb05b4c5e8ba9adcf1770e9a8226d4eb9478a
kshultz619 <kevinshultz619@gmail.com>
2024-11-20 15:32:18 -0800

    Update core.py

--- core.py
+++ core.py
@@ -4,7 +4,7 @@ import os
 
 # File to store the wish list
 DATA_FILE = "wish_list.csv"
-MUSIC_FILE = "MUSIC_FILE = "https://drive.google.com/uc?export=download&id=1-kPl_t-G9j4Vxa_AlfbsiAn4wZO6tF4B"
+MUSIC_FILE = "https://drive.google.com/uc?export=download&id=1-kPl_t-G9j4Vxa_AlfbsiAn4wZO6tF4B"
 
 
 # "https://drive.google.com/uc?export=download&id=1-kPl_t-G9j4Vxa_AlfbsiAn4wZO6tF4B"
